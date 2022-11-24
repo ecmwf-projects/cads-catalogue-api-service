fef5b41b0308c1aee0a296a5b8d6f36444750124
Luca Fabbri <l.fabbri@bopen.eu>
2022-11-24 15:35:21 +0100

    COPDS-538 - Integration of the layout.json (#20)
    * Integrated a static example of the layout integration

--- cads_catalogue_api_service/client.py
+++ cads_catalogue_api_service/client.py
@@ -291,6 +291,17 @@ def generate_collection_links(
             }
         )
 
+        if model.layout:
+            additional_links.append(
+                {
+                    "rel": "layout",
+                    "href": urllib.parse.urljoin(
+                        config.settings.document_storage_url, model.layout
+                    ),
+                    "type": "application/json",
+                }
+            )
+
         # Related datasets
         additional_links += [
             {
