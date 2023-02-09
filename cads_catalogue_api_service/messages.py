13e4c7df6694e72a1a2ea04699b536f4ed9cb583
Tosto Concetta <c.tosto@bopen.eu>
2023-02-09 16:48:52 +0100

    Add global messages and changelog

--- cads_catalogue_api_service/messages.py
+++ cads_catalogue_api_service/messages.py
@@ -22,7 +22,7 @@ import sqlalchemy as sa
 from . import dependencies, models
 
 router = fastapi.APIRouter(
-    prefix="/collections/{collection_id}/messages",
+    prefix="",
     tags=["messages"],
     responses={fastapi.status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
 )
@@ -74,7 +74,55 @@ def query_changelog_list(session_maker: sa.orm.Session) -> list[object]:
     return results
 
 
-@router.get("", response_model=models.Messages)
+@router.get("/collections/{collection_id}/messages", response_model=models.Messages)
+async def list_messages_by_id(
+    session_maker=fastapi.Depends(dependencies.get_session),
+) -> models.Message:
+    """Endpoint to get all messages of a specific collection."""
+    results = query_messages(session_maker)
+    return models.Messages(
+        messages=[
+            models.Message(
+                id=message["id"],
+                date=message["date"],
+                summary=message["summary"],
+                url=message["url"],
+                severity=message["severity"],
+                entries=message["entries"],
+                live=message["live"],
+            )
+            for message in results
+        ]
+    )
+
+
+@router.get(
+    "/collections/{collection_id}/messages/changelog",
+    response_model=models.ChangelogList,
+)
+async def list_changelog_by_id(
+    session_maker=fastapi.Depends(dependencies.get_session),
+) -> models.ChangelogList:
+    """Endpoint to get all changelog of a specific collection."""
+    results = query_changelog_list(session_maker)
+    return models.ChangelogList(
+        changelog=[
+            models.Changelog(
+                id=changelog["id"],
+                date=changelog["date"],
+                summary=changelog["summary"],
+                url=changelog["url"],
+                severity=changelog["severity"],
+                entries=changelog["entries"],
+                live=changelog["live"],
+                status=changelog["status"],
+            )
+            for changelog in results
+        ]
+    )
+
+
+@router.get("/messages", response_model=models.Messages)
 async def list_messages(
     session_maker=fastapi.Depends(dependencies.get_session),
 ) -> models.Message:
@@ -96,7 +144,7 @@ async def list_messages(
     )
 
 
-@router.get("/changelog", response_model=models.ChangelogList)
+@router.get("/messages/changelog", response_model=models.ChangelogList)
 async def list_changelog(
     session_maker=fastapi.Depends(dependencies.get_session),
 ) -> models.ChangelogList:
