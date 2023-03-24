ff7274d16db43061ad80029d9244255a572c6b0a
Tosto Concetta <c.tosto@bopen.eu>
2023-03-24 10:19:28 +0100

    Add jsonschema for vocabularies + fix

--- tests/integration_20_messages.py
+++ tests/integration_20_messages.py
@@ -13,21 +13,29 @@ format_checking = jsonschema.FormatChecker(formats=["date", "date-time"])
 ref_mapping = {}
 
 
-with open(
-    os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        "..",
-        "schemas",
-        "messages.json",
-    ),
-    "r",
-) as f:
-    ref_mapping["/schemas/messages"] = json.load(f)
+for schema_def in ("messages", "changelog"):
+    with open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "..",
+            "schemas",
+            f"{schema_def}.json",
+        ),
+        "r",
+    ) as f:
+        ref_mapping[f"/schemas/{schema_def}"] = json.load(f)
 
 MessageValidator = jsonschema.validators.validator_for(ref_mapping["/schemas/messages"])
+ChangelogValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/changelog"]
+)
 
 message_validator = MessageValidator(
     schema=ref_mapping["/schemas/messages"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+changelog_validator = ChangelogValidator(
+    schema=ref_mapping["/schemas/changelog"],
     resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
 )
 
@@ -53,4 +61,4 @@ def test_changelog_messages() -> None:
     changelog_list = results.get("changelog")
 
     assert type(changelog_list) == list
-    assert message_validator.validate(results, ref_mapping["/schemas/messages"])
+    assert changelog_validator.validate(results, ref_mapping["/schemas/changelog"])
