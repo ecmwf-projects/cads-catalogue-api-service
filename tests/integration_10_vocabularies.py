edce19fcbf80dcaa781e7c66c9c00dcae1bd0775
Cettina <c.tosto@bopen.eu>
2023-03-27 13:51:35 +0200

    Copds 887 migrate integration tests to json schema (#37)
    migrate integration tests to json schema

--- tests/integration_10_vocabularies.py
+++ tests/integration_10_vocabularies.py
@@ -1,13 +1,45 @@
+import json
 import os
 
+import jsonschema
 import requests
 
 API_ROOT_PATH = os.environ.get("API_ROOT_PATH", "")
 API_ROOT_PATH = API_ROOT_PATH if API_ROOT_PATH.endswith("/") else f"{API_ROOT_PATH}/"
 
 
+ref_mapping = {}
+
+for schema_def in ("keywords", "licences"):
+    with open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "..",
+            "schemas",
+            f"{schema_def}.json",
+        ),
+        "r",
+    ) as f:
+        ref_mapping[f"/schemas/{schema_def}"] = json.load(f)
+
+KeywordsValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/keywords"]
+)
+LicencesValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/licences"]
+)
+
+keyword_validator = KeywordsValidator(
+    schema=ref_mapping["/schemas/keywords"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+licence_validator = LicencesValidator(
+    schema=ref_mapping["/schemas/licences"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+
+
 def test_licences_vocabulary() -> None:
-    # TODO: replace with a JSON schema
     r = requests.get(f"{API_ROOT_PATH}vocabularies/licences")
 
     assert r.status_code == 200
@@ -16,15 +48,10 @@ def test_licences_vocabulary() -> None:
     licences = results.get("licences")
 
     assert type(licences) == list
-    for licence in licences:
-        assert type(licence) == dict
-        assert type(licence.get("id")) == str
-        assert type(licence.get("label")) == str
-        assert type(licence.get("revision")) == int
+    assert licence_validator.validate(results, ref_mapping["/schemas/licences"])
 
 
 def test_keywords_vocabulary() -> None:
-    # TODO: replace with a JSON schema
     r = requests.get(f"{API_ROOT_PATH}vocabularies/keywords")
 
     assert r.status_code == 200
@@ -33,7 +60,4 @@ def test_keywords_vocabulary() -> None:
     licences = results.get("keywords")
 
     assert type(licences) == list
-    for licence in licences:
-        assert type(licence) == dict
-        assert type(licence.get("id")) == str
-        assert type(licence.get("label")) == str
+    assert keyword_validator.validate(results, ref_mapping["/schemas/keywords"])
--- tests/integration_20_messages.py
+++ tests/integration_20_messages.py
@@ -1,13 +1,46 @@
+import json
 import os
 
+import jsonschema
 import requests
 
 API_ROOT_PATH = os.environ.get("API_ROOT_PATH", "")
 API_ROOT_PATH = API_ROOT_PATH if API_ROOT_PATH.endswith("/") else f"{API_ROOT_PATH}/"
 
 
+format_checking = jsonschema.FormatChecker(formats=["date", "date-time"])
+
+ref_mapping = {}
+
+
+for schema_def in ("messages", "changelog"):
+    with open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "..",
+            "schemas",
+            f"{schema_def}.json",
+        ),
+        "r",
+    ) as f:
+        ref_mapping[f"/schemas/{schema_def}"] = json.load(f)
+
+MessageValidator = jsonschema.validators.validator_for(ref_mapping["/schemas/messages"])
+ChangelogValidator = jsonschema.validators.validator_for(
+    ref_mapping["/schemas/changelog"]
+)
+
+message_validator = MessageValidator(
+    schema=ref_mapping["/schemas/messages"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+changelog_validator = ChangelogValidator(
+    schema=ref_mapping["/schemas/changelog"],
+    resolver=jsonschema.RefResolver("", {}, store=ref_mapping),
+)
+
+
 def test_messages() -> None:
-    # TODO: replace with a JSON schema
     r = requests.get(f"{API_ROOT_PATH}messages")
 
     assert r.status_code == 200
@@ -16,20 +49,10 @@ def test_messages() -> None:
     messages = results.get("messages")
 
     assert type(messages) == list
-    for message in messages:
-        assert type(message) == dict
-        assert type(message.get("id")) == str
-        assert type(message.get("date")) == str
-        assert type(message.get("summary")) == str or message.get("summary") is None
-        assert type(message.get("url")) == str or message.get("url") is None
-        assert type(message.get("severity")) == str
-        assert type(message.get("content")) == str
-        assert type(message.get("live")) == bool
-        assert type(message.get("status")) == str
+    assert message_validator.validate(results, ref_mapping["/schemas/messages"])
 
 
 def test_changelog_messages() -> None:
-    # TODO: replace with a JSON schema
     r = requests.get(f"{API_ROOT_PATH}messages/changelog")
 
     assert r.status_code == 200
@@ -38,13 +61,4 @@ def test_changelog_messages() -> None:
     changelog_list = results.get("changelog")
 
     assert type(changelog_list) == list
-    for changelog in changelog_list:
-        assert type(changelog) == dict
-        assert type(changelog.get("id")) == str
-        assert type(changelog.get("date")) == str
-        assert type(changelog.get("summary")) == str or changelog.get("summary") is None
-        assert type(changelog.get("url")) == str or changelog.get("url") is None
-        assert type(changelog.get("severity")) == str
-        assert type(changelog.get("content")) == str
-        assert type(changelog.get("live")) == bool
-        assert type(changelog.get("status")) == str
+    assert changelog_validator.validate(results, ref_mapping["/schemas/changelog"])
