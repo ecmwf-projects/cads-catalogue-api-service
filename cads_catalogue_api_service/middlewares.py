e3d7af0b7b344736e121457899191e771970f89b
Luca Fabbri <l.fabbri@bopen.eu>
2025-02-06 11:28:27 +0100

    Raise http cache to 3 minutes

--- cads_catalogue_api_service/middlewares.py
+++ cads_catalogue_api_service/middlewares.py
@@ -100,5 +100,5 @@ class CacheControlMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
             "cache-control" not in response.headers
             and request.method in CACHEABLE_HTTP_METHODS
         ):
-            response.headers.update({"cache-control": "public, max-age=300"})
+            response.headers.update({"cache-control": "public, max-age=180"})
         return response
