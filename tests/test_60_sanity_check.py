617daf3e911c3106cce7aef8476df9e60a118b52
Alfredo Toselli <alfredotoselli0@gmail.com>
2025-08-01 14:37:44 +0200

    test: expired sanity_check & update_frequency

--- tests/test_60_sanity_check.py
+++ tests/test_60_sanity_check.py
@@ -1,4 +1,5 @@
 import datetime
+from unittest import mock
 
 from cads_catalogue_api_service.sanity_check import (
     SanityCheckOutput,
@@ -267,4 +268,58 @@ def test_process() -> None:
     assert process(three_tests_none_success) == SanityCheckResult(
         status=SanityCheckStatus.down,
         timestamp=timestamp,
+    )
+
+
+@mock.patch(
+    "cads_catalogue_api_service.sanity_check.os.getenv",
+    return_value="10",
+)
+def test_process_expired(mock_getenv) -> None:
+    # Test case: expired status
+    expired_timestamp = datetime.datetime.now(
+        datetime.timezone.utc
+    ) - datetime.timedelta(minutes=11)
+    expired_test = [
+        SanityCheckOutput(
+            req_id="test1",
+            success=True,
+            started_at=expired_timestamp,
+            finished_at=expired_timestamp,
+        )
+    ]
+    assert process(expired_test) == SanityCheckResult(
+        status=SanityCheckStatus.expired,
+        timestamp=expired_timestamp,
+    )
+
+    # Test case: not expired status
+    not_expired_timestamp = datetime.datetime.now(
+        datetime.timezone.utc
+    ) - datetime.timedelta(minutes=9)
+    not_expired_test = [
+        SanityCheckOutput(
+            req_id="test1",
+            success=True,
+            started_at=not_expired_timestamp,
+            finished_at=not_expired_timestamp,
+        )
+    ]
+    assert process(not_expired_test) == SanityCheckResult(
+        status=SanityCheckStatus.available,
+        timestamp=not_expired_timestamp,
+    )
+
+    # Test case: env var is not set
+    mock_getenv.return_value = None
+    assert process(expired_test) == SanityCheckResult(
+        status=SanityCheckStatus.available,
+        timestamp=expired_timestamp,
+    )
+
+    # Test case: env var is not a valid value
+    mock_getenv.return_value = "not-a-valid-int"
+    assert process(expired_test) == SanityCheckResult(
+        status=SanityCheckStatus.available,
+        timestamp=expired_timestamp,
     )
